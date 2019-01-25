b34ebfc69b97d7356cfa848714890b4b1fa6bb28
Karim Saleh <admin@dandywaves.com>
2019-01-25 05:28:34 +0400

    Angulat Deploy

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
+import { LoginComponent } from './login/login.component';
+import { ShoppingCardComponent } from './shopping-card/shopping-card.component';
+import { AdminProductsComponent } from './admin/admin-products/admin-products.component';
+import { AdminOrdersComponent } from './admin/admin-orders/admin-orders.component';
+import { MyorderComponent } from './myorder/myorder.component';
+import { ProductComponent } from './product/product.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: ProductComponent },
+  { path: 'products', component: ProductComponent },
+  { path: 'orders', component: MyorderComponent },
+  { path: 'admin/orders', component: AdminOrdersComponent },
+  { path: 'admin/products', component: AdminProductsComponent },
+  { path: 'shoppingcard', component: ShoppingCardComponent },
+  { path: 'sign-in', component: LoginComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './layouts/nav-bar/nav-bar.component';
+import { ProductComponent } from './product/product.component';
+import { MyorderComponent } from './myorder/myorder.component';
+import { AdminProductsComponent } from './admin/admin-products/admin-products.component';
+import { AdminOrdersComponent } from './admin/admin-orders/admin-orders.component';
+import { ShoppingCardComponent } from './shopping-card/shopping-card.component';
+import { FooterComponent } from './layouts/footer/footer.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    ProductComponent,
+    MyorderComponent,
+    AdminProductsComponent,
+    AdminOrdersComponent,
+    ShoppingCardComponent,
+    FooterComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
